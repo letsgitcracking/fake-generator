3c8c7b7804fadfc8342bd347616335fcac9e8efd
letsgitcracking <digitalmailsecure@gmail.com>
2019-05-18 20:14:14 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const app = require('express')(),
+  cors = require('cors'),
+  faker = require('faker');
+
+app.set('port', process.env.PORT || 9000);
+
+//Ask - Do I need to use cors?
+app.use(cors());
+
+app.get('/data', (req, res) => {
+  res.json(getCustomCard())
+});
+
+getCustomCard = () => {
+  return {
+    "name": faker.name.findName(),
+    "address": {
+      "street": faker.address.streetName(true),
+      "suite": faker.address.secondaryAddress(),
+      "city": faker.address.city(),
+      "zipcode": faker.address.zipCode(),
+      "geo": {
+        "lat": faker.address.latitude(),
+        "lng": faker.address.longitude()
+      }
+    },
+    "dob": faker.date.past(50, new Date("Sat Sep 20 1992 21:35:02 GMT+0200 (CEST)")),
+    "motherMaiden": faker.name.lastName(),
+    "username": faker.internet.userName(),
+    "email": faker.internet.email(),
+    "phone": faker.phone.phoneNumber(),
+    "website": faker.internet.domainName(),
+    "company": {
+      "name": faker.company.companyName(),
+      "catchPhrase": faker.company.catchPhrase(),
+      "bs": faker.company.bs()
+    },
+    "avatar": faker.internet.avatar()
+  };
+};
+
+
+app.listen(app.get('port'), () => console.log('Server up: http://localhost:' + app.get('port')));
---
+++ script.js
@@ -0,0 +1,17 @@
+const generate = async () => {
+    const response = await fetch('http://localhost:9000/data');
+    const data = await response.json(); //extract JSON from the http response
+    document.getElementById('name').innerHTML = data.name;
+    document.getElementById('address1').innerHTML = data.address.suite + " " + data.address.street;
+    document.getElementById('address2').innerHTML = data.address.city + " " + data.address.zipcode;
+    document.getElementById('dob').innerHTML = data.dob.slice(0,10);
+    document.getElementById('maidenName').innerHTML = data.motherMaiden;
+    document.getElementById('email').innerHTML = data.email;
+    document.getElementById('phone').innerHTML = data.phone;
+    let image = document.createElement('img');
+    image.setAttribute("src", data.avatar);
+    document.body.appendChild(image);
+
+
+
+  }
