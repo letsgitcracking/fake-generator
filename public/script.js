501cc3f09582868344856e24a45e393932001c08
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2020-02-25 10:57:14 +0000

    Added icons.

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ const generate = async () => {
     document.getElementById('address1').innerHTML = data.address.suite + " " + data.address.street;
     document.getElementById('address2').innerHTML = data.address.city + " " + data.address.zipcode;
     document.getElementById('dob').innerHTML = data.dob.slice(0,10);
-    document.getElementById('maidenName').innerHTML = data.motherMaiden;
+    // document.getElementById('maidenName').innerHTML = data.motherMaiden;
     document.getElementById('email').innerHTML = data.email;
     document.getElementById('phone').innerHTML = data.phone;
     document.getElementById('avatar').src = data.avatar;
